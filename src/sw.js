616015f6d2fee002186540505d2734fc1a2e6f21
Sky Chen <sky.chen@salesforce.com>
2023-02-25 03:10:48 -0800

    add voice support

--- src/sw.js
+++ src/sw.js
@@ -1,6 +1,6 @@
 // Import the Workbox library
-import { precacheAndRoute } from 'workbox-precaching';
-import { routes } from './components/common/routes';
+// import { precacheAndRoute } from 'workbox-precaching';
+// import { routes } from './components/common/routes';
 
-// Call precacheAndRoute with an array of all your client-side URLs
-precacheAndRoute(Object.keys(routes));
+// // Call precacheAndRoute with an array of all your client-side URLs
+// precacheAndRoute(Object.keys(routes));
