947e599ec9b2df4ea9ad8212f5e4c0897cd6f6ba
Sky Chen <sky.chen@salesforce.com>
2023-03-02 17:18:45 -0800

    prompt engineering

--- src/components/workgpt/workTemplates.js
+++ src/components/workgpt/workTemplates.js
@@ -1,4 +1,98 @@
 const workTemplates = {
+    'Parameter Generation': {
+        workContext: `
+Manage the value setting of the following variables while chatting with me:
+metric = 'NA' initially, it can be set to value of ['revenue', 'unit']
+group_by = 'NA' initially, it can be set to value of ['customer', 'city', 'country', 'continent', 'day', 'month', 'quarter', 'year']
+customer = 'NA' initially, it can be set to value of any company name
+location = 'NA' initially, it can be set to any continent, country, city
+start_date = '1/1/2022' initially, it can be set to any date
+
+Q: How are you?
+A: show(metric='NA', group_by='NA', customer='NA', location='NA', start_date-'1/1/2022')
+
+Q: Show my revenue since March 2022
+A: show(metric='revenue', group_by='NA', customer='NA', location='NA', start_date-'3/1/2022')
+
+Q: Breakdown by city
+A: show(metric='revenue', group_by='city', customer='NA', location='NA', start_date-'3/1/2022')
+
+
+Q: Group by month
+A: show(metric='revenue', group_by='month', customer='NA', location='NA', start_date-'3/1/2022')
+
+
+Q: Drill down to Walmart
+A: show(metric='revenue', group_by='month', customer='Walmart', location='NA', start_date-'3/1/2022')
+
+Q: Zoom in to China
+A: show(metric='revenue', group_by='month', customer='Walmart', location='China', start_date-'3/1/2022')
+
+
+Q: Let me think more about it
+A: show(metric='revenue', group_by='month', customer='Walmart', location='China', start_date-'3/1/2022')
+        `,
+        actions: {
+            'Execute getChart': {
+                description: 'Execute the getChart()',
+                command: (parameters) => {
+                    console.log(`Executing getChart command for parameters: ${parameters}`);
+                },
+                processResult: (result, error) => {
+                    if (error) {
+                        console.error(`Failed to execute getChart command, error is ${error}`);
+                    }
+
+                    console.log(`Processed getChart result: ${result}`);
+                }
+            }
+        }
+    },
+
+
+
+
+    'Generic Workflow': {
+        workContext: `
+# Overall WorkFlow
+
+This is the top level workflow, which you can see all the workflows that Human can perform below.
+
+If you cannot find any workflow match, come back to this <Overall Workflow>
+
+## Available Workflows
+
+The list of available workflows are listed below:
+
+### Analytics Copilot
+
+
+### API Parameters Generation
+
+
+### API execution
+
+
+### 
+
+
+        `,
+        actions: {
+            'Execute getChart': {
+                description: 'Execute the getChart()',
+                command: (parameters) => {
+                    console.log(`Executing getChart command for parameters: ${parameters}`);
+                },
+                processResult: (result, error) => {
+                    if (error) {
+                        console.error(`Failed to execute getChart command, error is ${error}`);
+                    }
+
+                    console.log(`Processed getChart result: ${result}`);
+                }
+            }
+        }
+    },
     'Analytics Copilot': {
         workContext: `
 <Tables>:
