2ee87e9a303b8fc0fad5c42f8701f4a228a82dc6
Sky Chen <sky.chen@salesforce.com>
2023-03-18 21:20:49 -0700

    handle offline

--- src/components/Auth/Callback.js
+++ src/components/Auth/Callback.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Typography from "@mui/material/Typography";
 
 class Callback extends Component {
   componentDidMount = () => {
@@ -10,7 +13,15 @@ class Callback extends Component {
     }
   };
   render() {
-    return <h1>Loading...</h1>;
+    return (
+      <Container component="main" sx={{ mb: 4 }}>
+          <Paper variant="outlined" sx={{ my: { xs: 2, md: 6 }, p: { xs: 2, md: 3 } }}>
+              <Typography component="h2">
+                  Loading
+              </Typography>
+          </Paper>
+      </Container>
+    );
   }
 }
 
